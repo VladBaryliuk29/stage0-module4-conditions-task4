0683ff27c708b58782b70aa9f7670414352aa3e5
unknown <vladmain29@gmail.com>
2023-02-25 15:11:12 +0300

    tasks for mjc

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
+    public void stop(){}
     public void printGreatest(int first, int second, int third) {
+        int greatest = first >= second && first >= third
+                ? first:
+                Math.max(second, third);
+        System.out.println(greatest);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest = first >= second && first >= third
+                ? first:
+                Math.max(second, third);
+        System.out.println(greatest);
     }
 }
